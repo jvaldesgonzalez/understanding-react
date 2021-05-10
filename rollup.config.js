06e7a7e7350d1c9cdedc0d2a271262ac86f09ef9
jvaldesgonzalez <jvaldesgonzalez9@gmail.com>
2021-05-10 00:24:59 -0400

    feat(rollup): build rollup.config.js
    rollup.js config file with hot reload and development server

---
+++ rollup.config.js
@@ -0,0 +1,31 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import serve from "rollup-plugin-serve";
+import livereload from "rollup-plugin-livereload";
+import babel from "@rollup/plugin-babel";
+import replace from "@rollup/plugin-replace";
+
+export default {
+  input: "src/main.js",
+  output: {
+    file: "public/bundle.js",
+    format: "iife",
+    sourcemap: true,
+  },
+  plugins: [
+    resolve(),
+    commonjs(),
+    babel({ babelHelpers: "bundled", presets: ["@babel/preset-env"] }),
+    replace({
+      "process.env.NODE_ENV": JSON.stringify("development"),
+    }),
+    serve({
+      open: true,
+      verbose: true,
+      contentBase: ["public"],
+      host: "localhost",
+      port: 7000,
+    }),
+    livereload({ watch: "public" }),
+  ],
+};
